d4c4ea363d48cce772e96847d00e71d34839a08e
성현 이 <leetjdgus@gmail.com>
2026-04-09 11:35:15 +0900

    과제 1 코딩과 테스트 완료 버거 키오스크의 기본 틀을 잡아 기본적인 코딩까지 마쳤다.

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ namespace BurgerKiosk
             InitializeComponent();
         }
 
+        public int totalCost = 0;
+
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -19,8 +21,56 @@ namespace BurgerKiosk
 
         private void btnOrder_Click(object sender, EventArgs e)
         {
+            
 
-        }
+            if (rdoBulgogiBurger.Checked)
+            {
+                totalCost += 4000;
+                lstOrder.Items.Add("불고기버거 4,000원");
+
+            }
+            if (rdoChickenBurger.Checked)
+            {
+                totalCost += 3000;
+                lstOrder.Items.Add("치킨버거 3,000원");
+            }
+
+ 
+            if (chkCoke.Checked) { 
+                totalCost += 2500;
+                lstOrder.Items.Add("콜라 추가 2,500원");
+            }
+            if (chkCheese.Checked) { 
+                totalCost += 1500;
+                lstOrder.Items.Add("치즈 추가 1,500원");
+            }
+            if (chkSauce.Checked) { 
+                totalCost += 500;
+                lstOrder.Items.Add("소스 추가 500원");
+            }
+
+            if (rdoHamBurger.Checked)
+            {
+                totalCost += 5000;
+                lstOrder.Items.Add("햄버거 5,000원");
+            }
+
+            if (chkFries.Checked)
+            {
+                totalCost += 3500;
+                lstOrder.Items.Add("감자튀김 3,500원");
+
+                
+            }
+            if (!rdoHamBurger.Checked && !rdoBulgogiBurger.Checked 
+                                            && !rdoChickenBurger.Checked) 
+            { 
+                MessageBox.Show("메뉴를선택하세요."); 
+                return; 
+            }
+            lblTotalCost.Text = "총금액: " + totalCost + "원";
+        }   
+            
 
         private void btnReset_Click(object sender, EventArgs e)
         {
